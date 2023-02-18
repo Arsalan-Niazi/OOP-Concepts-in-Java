8a55a3dafca454887cf5e5eb885a0e9c22d0f1aa
Arsalan Niazi <91112959+Arsalan-Niazi@users.noreply.github.com>
2023-02-18 09:45:04 +0500

    Add files via upload

---
+++ LAB-1/Addition.java
@@ -0,0 +1,8 @@
+class Addition {
+public static void main(String[] args) {
+int a,b,c; //this statement declares three variables a, b and c.
+a=2;
+b=3;
+c=a+b;
+System.out.println("Sum of two numbers = "+c); }
+}
---
+++ LAB-1/Basic1.java
@@ -0,0 +1,9 @@
+public class Basic1 {
+//your program begins with a call to main()
+public static void main(String[] args) {
+System.out.println("Welcome to Java World!");
+System.out.println("Name: Muhammad Arsalan");
+System.out.println("Reg :20-SE-56");
+//println() displays the string which is passed to it.
+}
+}
